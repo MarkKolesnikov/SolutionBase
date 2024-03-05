525e1b88d13b98011548ca806e615238f0d86126
Your Name <azzytm@yandex.ru>
2024-03-05 16:59:17 +0300

    add solution

--- src/solution/sleepIn/SleepIn.java
+++ src/solution/sleepIn/SleepIn.java
@@ -1,7 +1,18 @@
 package solution.sleepIn;
 
+import java.util.Scanner;
+
 public class SleepIn {
     public static void main(String[] args) {
-        
+
+        System.out.println("Input true/false for sleeping in: ");
+        boolean sleepIn = new Scanner(System.in).nextBoolean();
+        System.out.println("Input true/false for weekday: ");
+        boolean weekday = new Scanner(System.in).nextBoolean();
+        if (sleepIn && !weekday) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
