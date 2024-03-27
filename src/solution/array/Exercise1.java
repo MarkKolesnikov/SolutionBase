bc4895b4685ef140f4762ed83880cf6c16f11bd1
Your Name <azzytm@yandex.ru>
2024-03-27 20:54:53 +0300

    comÐÐ

--- src/solution/array/Exercise1.java
+++ src/solution/array/Exercise1.java
@@ -12,7 +12,7 @@ public class Exercise1 {
         };
 
         // Declare and initialize a string array.
-        String[] my_array2 = {
+        String[] my_array2 = { // 0 1 2 3 4 5 6 7 8 9 10 11 12 13
                 "Java",
                 "Python",
                 "PHP",
